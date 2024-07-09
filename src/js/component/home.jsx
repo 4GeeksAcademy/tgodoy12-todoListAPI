0b72415c6333e3226ec98ece911d4a6552000734
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-07-09 19:24:31 +0000

    user created with creatUser(), and getMyList() added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Input from "./input";
 import List from "./list";
 
@@ -6,21 +6,50 @@ import List from "./list";
 const Home = () => {
 
 	const [task, setTask] = useState("");
-	const [listTask, setListTask] = useState([])
-
+	const [listTask, setListTask] = useState([]);
+	
+	//read the task
 	const addTask = (e) => {
 		setTask(e.target.value);
 		
 	}
-
+	//add a task to the array
 	const addListTask = (e) => {
 		if(e.keyCode === 13 && e.key !== "") {
 			setListTask(listTask.concat(e.target.value))
 			setTask("");
-		}
-		
+		}	
 	}
 	console.log(listTask);
+
+	//conection with the todo api
+	//create user with POST method
+
+	// const createUser = () => {
+	// 	fetch('https://playground.4geeks.com/todo/users/tgodoy', {
+	// 		method: "POST",
+	// 		headers: {
+	// 			'Content-Type': 'application/json'
+	// 		}
+	// 	})
+	// 	.then(response => response.json())
+	// 	.then(response => console.log(response))
+	// 	.catch(error => console.log(error));
+	// }
+
+	//get my user toDos array 
+	const getMyList = () => {
+		fetch('https://playground.4geeks.com/todo/users/tgodoy', {
+		})
+		.then(response => response.json())
+		.then(response =>setListTask(response.todos))
+		.catch(error => console.log(error));
+	}
+
+	//each time my page is loaded, my user task list is shown 
+	useEffect(() => {
+		getMyList();
+	},[]);
 
 	return (
 		<div className="container">
--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -6,7 +6,7 @@ const List = (props) => {
     return (
         <ul className="list-group list-group-flush">
             {props.taskAdded.map((element, index) => (            
-                <li key={index} className="list-group-item d-flex justify-content-between">{element}<Delete id={index} taskList={props.taskAdded} setTask={props.setTask}/></li>
+                <li key={index} className="list-group-item d-flex justify-content-between">{element.label}<Delete id={index} taskList={props.taskAdded} setTask={props.setTask}/></li>
             ))}         
         </ul>
     ) 
